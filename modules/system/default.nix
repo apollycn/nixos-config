09b01699c32dfe14007a75a31ceeff5650526755
leo <apollycn@gmail.com>
2025-08-02 20:05:30 +0000

    add default.nix in systems module

---
+++ modules/system/default.nix
@@ -0,0 +1,28 @@
+{ inputs, pkgs, ... }:
+
+{
+    imports =
+	[
+	    ./hyprland.nix
+	    ./graphics.nix
+            ./boot.nix
+	    ./network.nix
+	    ./sound.nix
+	    ./users/default.nix
+	];
+
+    time.timeZone = "America/New_York";
+
+    services.printing.enable = true;
+
+    services.libinput.enable = true;
+
+    security.sudo.enable = true;
+
+    programs.mtr.enable = true;
+    programs.gnupg.agent = {
+	enable = true;
+	enableSSHSupport = true;
+    services.openssh.enable = true;
+}
+

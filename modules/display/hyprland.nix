13de12444532010e460ae68d193e252eb301e37f
leo <apollycn@gmail.com>
2025-08-02 01:38:28 +0000

    add minimal hyprland settings

--- modules/display/hyprland.nix
+++ modules/display/hyprland.nix
@@ -5,5 +5,21 @@
 	systemd.enable = true;
 	package = null;
 	portalPackage = null;
+
+	settings = {
+	    "$mod" = "SUPER";
+	    bind = [
+		"$mod, F, exec, firefox"
+	    ]
+	    ++ (
+		builtins.concatLists (builtins.getList (i:
+		    let ws = i + 1;
+		    in [
+			"$mod, code:1${toString i}, workspace, ${toString ws}"
+			"$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+		    ]
+		)
+	    9)
+	};
     };
 }

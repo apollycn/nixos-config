f48b9b56aa7b91bda44de578be26c5c9ecb0f89b
leo <apollycn@gmail.com>
2025-08-02 01:43:38 +0000

    fix typo builtins.genList

--- modules/display/hyprland.nix
+++ modules/display/hyprland.nix
@@ -12,7 +12,7 @@
 		"$mod, F, exec, firefox"
 	    ]
 	    ++ (
-		builtins.concatLists (builtins.getList (i:
+		builtins.concatLists (builtins.genList (i:
 		    let ws = i + 1;
 		    in [
 			"$mod, code:1${toString i}, workspace, ${toString ws}"

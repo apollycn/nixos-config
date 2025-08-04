7ffbeed5244cbb4b0e0e648b5b44ef2f6b989484
leo <apollycn@gmail.com>
2025-08-04 16:45:17 -0400

    add fonts

--- modules/environment/default.nix
+++ modules/environment/default.nix
@@ -3,5 +3,6 @@
 {
     imports = [
 	./variables.nix
+	./fonts.nix
     ];
 }
---
+++ modules/environment/fonts.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+
+{
+    fonts.packages = with pkgs; [
+	jetbrains-mono
+	nerd-font-patcher
+	noto-fonts-color-emoji
+    ];
+}

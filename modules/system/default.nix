063b35f512af3786e0e90bb00414c279e417f3c3
leo <apollycn@gmail.com>
2025-08-04 15:26:23 -0400

    add bluetooth config

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,28 +1,11 @@
-{ inputs, pkgs, ... }:
+{ ... }:
 
 {
     imports =
 	[
-	    ./hyprland.nix
 	    ./graphics.nix
             ./boot.nix
 	    ./network.nix
 	    ./sound.nix
 	];
-
-    time.timeZone = "America/New_York";
-
-    services.printing.enable = true;
-
-    services.libinput.enable = true;
-
-    security.sudo.enable = true;
-
-    programs.mtr.enable = true;
-    programs.gnupg.agent = {
-	enable = true;
-	enableSSHSupport = true;
-    };
-    services.openssh.enable = true;
 }
-

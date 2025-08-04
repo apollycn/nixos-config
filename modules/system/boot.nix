07cede27b469b391945e14cd84583fe998afc384
leo <apollycn@gmail.com>
2025-08-04 15:45:56 -0400

    add more configs to boot.nix

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,8 +1,26 @@
-{ input, pkgs, ... }:
+{ pkgs, ... }:
 
 {
     boot.loader = {
 	systemd-boot.enable = true;
 	efi.canTouchEfiVariables = true;
+	timeout = 10;
+    };
+
+    boot.initrd = {
+	enable = true;
+	verbose = false;
+	systemd.enable = true;
+	availableKernelModules = [ "i915" ];
+	kernelModules = [ "i915" ];
+    };
+
+    boot.consoleLogLevel = 3;
+    
+    boot.plymouth = {
+	enable = true;
+	font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono.Regular.ttf";
+	themePackages = [ pkgs.catppuccin-plymouth ];
+	theme = "catppuccin-macchiato";
     };
 }

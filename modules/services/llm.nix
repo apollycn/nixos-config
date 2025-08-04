dd451c5c2b6184f050569deaa30ed7ae91a65fa0
leo <apollycn@gmail.com>
2025-08-04 17:30:42 -0400

    add llm services configs

---
+++ modules/services/llm.nix
@@ -0,0 +1,48 @@
+{ pkgs, config, ... }:
+
+{
+    services.ollama = {
+    	enable = true;
+    	loadModels = [ "llama3.2:3b" "phi4-reasoning:14b" "dolphin3:8b" "smallthinker:3b" "gemma3n:e4b" "deepcoder:14b" "qwen3:14b" "qwen3-coder:30b"  "nomic-embed-text" ];
+    	acceleration = "cuda";
+    };
+
+    services.searx = {
+    	enable = true;
+    	settings = {
+   	    server = {
+                port = 7777;
+            	bind_address = "127.0.0.1";
+            	secret_key = "@SEARX_SECRET_KEY@";
+            };
+            search = {
+                formats = [ "html" "json" ];
+            };
+        };
+        environmentFile = "${config.users.users.leo.home}/.config/.env.searxng"; 
+    };
+
+    services.n8n = {
+        enable = true;
+    };
+
+    services.open-webui = {
+    	enable = true;
+    	port = 8888;
+    	host = "127.0.0.1";
+    };
+  
+    environment.systemPackages = with pkgs; [
+    	oterm
+    	alpaca
+
+    	aichat
+    	fabric-ai
+
+    	aider-chat
+    	opencode
+
+        chromium
+    	playwright
+    ];
+}

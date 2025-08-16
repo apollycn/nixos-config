c810b4f47638fa17960adb3df586f88ed5babbc5
leo <apollycn@gmail.com>
2025-08-16 18:02:00 +0000

    fix pkgs

--- modules/desktop/display-manager.nix
+++ modules/desktop/display-manager.nix
@@ -4,7 +4,7 @@
     services.greetd = {
 	enable = true;
 	settings = {
-	    command = "${pkgs.greetd.tuigreet}/bin/tuigreet \
+	    command = "${pkgs.tuigreet}/bin/tuigreet \
                 --time --time-format '%I:%M %p | %a • %h | %F' \
           	--cmd 'uwsm start hyprland'";
 	    user = "greeter";

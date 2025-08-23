138afc2815527c03e63f64db17d0e5da834a38c5
leo <apollycn@gmail.com>
2025-08-23 09:25:26 -0400

    fix tuigreet executable

--- modules/desktop/display-manager.nix
+++ modules/desktop/display-manager.nix
@@ -4,7 +4,7 @@
     services.greetd = {
 	enable = true;
 	settings.default_session = {
-	    command = "${pkgs.tuigreet} \
+	    command = "${pkgs.tuigreet}/bin/tuigreet \
                 --time --time-format '%I:%M %p | %a • %h | %F' \
           	--cmd 'uwsm start hyprland'";
 	    user = "greeter";

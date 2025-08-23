208447caa6528bbc8a8150aa874100d16844a0f9
leo <apollycn@gmail.com>
2025-08-23 09:11:43 -0400

    fix macchanger interface name

--- modules/security/mac-randomization.nix
+++ modules/security/mac-randomization.nix
@@ -8,8 +8,8 @@
 	after = [ "network.target" ];
 	serviceConfig = {
 	    Type = "oneshot";
-	    ExecStart = "${pkgs.macchanger}/bin/macchanger -r wlp0s20f3";
-      	    ExecStop = "${pkgs.macchanger}/bin/macchanger -p wlp0s20f3";
+	    ExecStart = "${pkgs.macchanger}/bin/macchanger -r wlan0";
+      	    ExecStop = "${pkgs.macchanger}/bin/macchanger -p wlan0";
       	    RemainAfterExit = true;
         };
     };

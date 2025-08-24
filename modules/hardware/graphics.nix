44a4c38c7f4811a4fe4630970e68f82740e033a3
leo <apollycn@gmail.com>
2025-08-23 23:40:12 -0400

    add mesa drivers

--- modules/hardware/graphics.nix
+++ modules/hardware/graphics.nix
@@ -2,30 +2,37 @@
 
 let
     pkgs-unstable = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-in {
+in
+{
     nixpkgs.config.packageOverrides = pkgs: {
-	intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+	      intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
     };
 
     hardware.graphics = {
-	enable = true;
-	enable32Bit = true;
+	      enable = true;
+	      enable32Bit = true;
 
-	package = pkgs-unstable.mesa;
-	package32 = pkgs-unstable.pkgsi686Linux.mesa;
+	      package = pkgs-unstable.mesa;
+	      package32 = pkgs-unstable.pkgsi686Linux.mesa;
 
-	extraPackages = with pkgs; [
+	      extraPackages = with pkgs; [
       	    intel-compute-runtime
       	    intel-media-driver    # LIBVA_DRIVER_NAME=iHD
       	    intel-vaapi-driver    # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       	    vaapiVdpau
       	    libvdpau-va-gl
-    	];
-    	extraPackages32 = with pkgs.pkgsi686Linux; [
+    	  ];
+    	  extraPackages32 = with pkgs.pkgsi686Linux; [
       	    intel-media-driver
             intel-vaapi-driver
       	    vaapiVdpau
             libvdpau-va-gl
-    	];
+    	  ];
     };
+
+    environment.systemPackages = with pkgs; [
+        mesa
+        mesa.drivers
+        libglvnd
+    ];
 }

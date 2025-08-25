615a2e2942139f20f59aa44f29a7afe4db62fcb0
leo <apollycn@gmail.com>
2025-08-25 14:10:24 -0400

    add fzf

--- modules/tools/terminal-utils.nix
+++ modules/tools/terminal-utils.nix
@@ -2,23 +2,24 @@
 
 {
     environment.systemPackages = with pkgs; [
-    	file
-    	git
-    	lazygit
-    	delta
-    	license-generator
-    	process-compose
-    	progress
-    	noti
-    	ripgrep
-    	procs
-    	tealdeer
-    	bat
-    	rsclock
-    	cava
-	    unzip
-    	eza
-    	texlive.combined.scheme-medium
-    	glow
+        file
+        git
+        lazygit
+        delta
+        license-generator
+        process-compose
+        progress
+        noti
+        ripgrep
+        procs
+        tealdeer
+        bat
+        rsclock
+        cava
+        unzip
+        eza
+        texlive.combined.scheme-medium
+        glow
+        fzf
     ];
 }

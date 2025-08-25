4c74476d2e4e3d4ffbfc8f9274a74a2ea35d83ab
leo <apollycn@gmail.com>
2025-08-24 20:07:22 -0400

    chore(dev): update development packages
    Remove unnecessary or outdated development packages and add new ones.
    
    - Removed:
    - vscode-extensions.vadimcn.vscode-lldb
    - emmet-language-server
    - docker-compose-language-service
    - hyprls
    - jdk11
    - helix-gpt
    - Added:
    - texlab

--- modules/development/lsp.nix
+++ modules/development/lsp.nix
@@ -2,23 +2,22 @@
 
 {
     environment.systemPackages = with pkgs; [
-    	python313Packages.python-lsp-server
-    	nodePackages_latest.nodemon
-    	nodePackages_latest.typescript
-    	nodePackages_latest.typescript-language-server
-    	nodePackages_latest.vscode-langservers-extracted
-    	nodePackages_latest.yaml-language-server
-    	nodePackages_latest.dockerfile-language-server-nodejs
-    	nodePackages_latest.bash-language-server
-    	sumneko-lua-language-server
-    	marksman
+    	  python313Packages.python-lsp-server
+    	  nodePackages_latest.nodemon
+    	  nodePackages_latest.typescript
+    	  nodePackages_latest.typescript-language-server
+    	  nodePackages_latest.yaml-language-server
+    	  nodePackages_latest.dockerfile-language-server-nodejs
+    	  nodePackages_latest.bash-language-server
+    	  sumneko-lua-language-server
+    	  marksman
         markdown-oxide
-    	nil
-    	emmet-language-server
-    	docker-compose-language-service
-    	vscode-extensions.vadimcn.vscode-lldb
-    	hyprls
-    	jdk11
-	helix-gpt
+    	  nil
+    	  emmet-language-server
+    	  docker-compose-language-service
+    	  hyprls
+        jdk11
+        helix-gpt
+        texlab
     ]; 
 }

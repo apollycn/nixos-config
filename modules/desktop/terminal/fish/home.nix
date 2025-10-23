d8fbf4b747d8b9f2830cbecdbc8cf28104328b22
leo <apollycn@gmail.com>
2025-10-22 22:31:40 -0400

    fix dupe

--- modules/desktop/terminal/fish/home.nix
+++ modules/desktop/terminal/fish/home.nix
@@ -107,7 +107,6 @@
     
         # Custom functions
         functions = {
-            autostart = builtins.readFile ./functions/autostart.fish;
             aichat_fish = builtins.readFile ./functions/aichat_fish.fish;
             airplane_mode_toggle = builtins.readFile ./functions/airplane_mode_toggle.fish;
             archive_preview = builtins.readFile ./functions/archive_preview.fish;
